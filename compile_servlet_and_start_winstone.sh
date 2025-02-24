1417d0b5aa5e7dd9cc5a2e8eee3d741fa237df45
Haimen <haimenabbas@gmail.com>
2025-02-24 16:00:27 +0100

    corrected web.xml servlet-class path

--- compile_servlet_and_start_winstone.sh
+++ compile_servlet_and_start_winstone.sh
@@ -6,5 +6,8 @@ then
     PATHSEP=";"
 fi
 
-javac -cp "www/WEB-INF/lib/org.json.jar:winstone.jar:www/WEB-INF/classes" www/WEB-INF/classes/se/yrgo/schedule/database/*.java && java -jar winstone.jar --webroot=www
+# javac -cp "www/WEB-INF/lib/org.json.jar:winstone.jar:www/WEB-INF/classes" www/WEB-INF/classes/se/yrgo/schedule/database/*.java && java -jar winstone.jar --webroot=www
 
+
+# Rätt script 
+javac -cp "www/WEB-INF/lib/*:winstone.jar:www/WEB-INF/classes" www/WEB-INF/classes/se/yrgo/schedule/*/*.java && java -jar winstone.jar --webroot=www
